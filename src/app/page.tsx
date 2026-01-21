11d7e06e763a2a46c10c27191d27858118221832
Jovan Monteiro <jovs@Jovans-MacBook-Air.local>
2026-01-20 23:21:12 -0600

    hero removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import { sanityClient } from "@/lib/sanity.client";
 import { POSTS_QUERY } from "@/lib/sanity.queries";
 import type { Post } from "@/lib/types";
 import { HomeView } from "@/components/HomeView";
-import { Hero } from "@/components/Hero";
 
 export default async function HomePage() {
   const posts = await sanityClient.fetch<Post[]>(POSTS_QUERY);
 
   return (
     <div className="space-y-10">
-      <Hero />
       <HomeView posts={posts} />
     </div>
   );
